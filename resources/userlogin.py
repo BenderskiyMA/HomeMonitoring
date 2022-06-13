412fc357937318b4a42aa4d843db8da9399231b1
BenderskiyMA <olmer@remlo.ru>
2022-06-13 22:09:08 +0700

    function safe_str_cmp was deprecated, Made own.

--- resources/userlogin.py
+++ resources/userlogin.py
@@ -2,9 +2,9 @@ from typing import Tuple
 
 from flask_restful import Resource
 from flask_jwt_extended import create_access_token, create_refresh_token
-from werkzeug.security import safe_str_cmp
 
 from model.user import UserModel
+from utils.functions import safe_str_cmp
 from utils.paramparsers import _user_parser
 
 
