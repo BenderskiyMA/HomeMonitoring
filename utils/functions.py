2eed7425a38191a43ecd92127014ebd8c2945767
BenderskiyMA <olmer@remlo.ru>
2022-06-13 22:09:08 +0700

    function safe_str_cmp was deprecated, Made own.

--- utils/functions.py
+++ utils/functions.py
@@ -1,11 +1,27 @@
 from typing import Union
 
 from flask_jwt_extended import get_jwt
-from psycopg2._psycopg import Boolean
+import hmac
 
 ERROR_ACCESS_DENIED = {"message": "Error! Access denied!"}
 
 PERIOD_TYPES = ["hour", "day", "week", "month"]
+
+
+def safe_str_cmp(a: str, b: str) -> bool:
+    """This function compares strings in somewhat constant time. This
+    requires that the length of at least one string is known in advance.
+
+    Returns `True` if the two strings are equal, or `False` if they are not.
+    """
+
+    if isinstance(a, str):
+        a = a.encode("utf-8")  # type: ignore
+
+    if isinstance(b, str):
+        b = b.encode("utf-8")  # type: ignore
+
+    return hmac.compare_digest(a, b)
 
 
 def isfloat(num: str) -> bool:
